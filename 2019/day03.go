6323c4e8dd1a14879dc1d1320d30b6e8ff0e8209
mugimaru73 <Mugimaru@live.ru>
2020-05-17 22:10:46 +0400

    add 2019-03

---
+++ 2019/day03.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/mugimaru73/adventofcode/executor"
+)
+
+type step struct {
+	dir string
+	len int
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func run(input string) (interface{}, interface{}, interface{}) {
+	firstPath, secondPath := parseInput(input)
+
+	var m = make(map[point]int)
+
+	walk(firstPath, m, false)
+	intersections, totalSteps := walk(secondPath, m, true)
+
+	minSteps := totalSteps[0]
+	minDistance := calculateDistance(intersections[0])
+	for i := 1; i < len(intersections); i++ {
+		distance := calculateDistance(intersections[i])
+		if minDistance > distance {
+			minDistance = distance
+		}
+
+		if minSteps > totalSteps[i] {
+			minSteps = totalSteps[i]
+		}
+	}
+
+	return minDistance, minSteps, nil
+}
+
+func calculateDistance(p point) int {
+	return abs(p.x) + abs(p.y)
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+
+	return v
+}
+
+func walk(plan []step, walked map[point]int, freeze bool) ([]point, []int) {
+	steps := 0
+	currentPoint := point{x: 0, y: 0}
+	var intersections []point
+	var totalSteps []int
+
+	for _, step := range plan {
+		for i := 0; i < step.len; i++ {
+			steps++
+			currentPoint = nextPoint(currentPoint, step.dir)
+
+			if walked[currentPoint] > 0 {
+				intersections = append(intersections, currentPoint)
+				totalSteps = append(totalSteps, walked[currentPoint]+steps)
+			} else {
+				if !freeze {
+					walked[currentPoint] = steps
+				}
+			}
+		}
+	}
+
+	return intersections, totalSteps
+}
+
+func nextPoint(currentPoint point, dir string) point {
+	newPoint := point{x: currentPoint.x, y: currentPoint.y}
+
+	switch dir {
+	case "L":
+		newPoint.x--
+	case "R":
+		newPoint.x++
+	case "U":
+		newPoint.y++
+	case "D":
+		newPoint.y--
+	}
+
+	return newPoint
+}
+
+func parseInput(rawInput string) ([]step, []step) {
+	pathsStr := strings.Split(rawInput, "\n")
+	return parsePath(pathsStr[0]), parsePath(pathsStr[1])
+}
+
+func parsePath(pathStr string) []step {
+	path := strings.Split(pathStr, ",")
+	parsed := make([]step, len(path))
+
+	for i, st := range path {
+		len, _ := strconv.Atoi(st[1:])
+		parsed[i] = step{dir: st[0:1], len: len}
+	}
+
+	return parsed
+}
+
+func main() {
+	executor.Run(executor.ReadInput("2019/day03.input.txt"), run)
+}
