12808e1cbf81591dbfa7ff35461fa87ac15c4160
mugimaru73 <Mugimaru@live.ru>
2020-05-17 16:12:02 +0400

    add 2019-01 solver

---
+++ 2019/day01.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/mugimaru73/adventofcode/executor"
+)
+
+func run(input string) (interface{}, interface{}, interface{}) {
+	var Part1 = 0.0
+	var Part2 = 0.0
+
+	for _, v := range strings.Split(input, "\n") {
+		distance, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		fuelReq := fuelRequired(distance)
+		Part1 += fuelReq
+		Part2 += part2FuelRequired(fuelReq, fuelReq)
+	}
+
+	return int(Part1), int(Part2), nil
+}
+
+func fuelRequired(distance float64) float64 {
+	return math.Trunc(distance/3) - 2.0
+}
+
+func part2FuelRequired(acc float64, distance float64) float64 {
+	var req = fuelRequired(distance)
+	if req < 0 {
+		req = 0
+	}
+
+	if distance-req > 0 {
+		acc = part2FuelRequired(acc+req, req)
+	}
+
+	return acc
+}
+
+func main() {
+	executor.Run(executor.ReadInput("2019/day01.input.txt"), run)
+}
